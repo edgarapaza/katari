c28575da61018a382e40ef8f4eb55bb47ecb50eb
Edgar Apaza Choque <edgarapazac@gmail.com>
2023-10-06 10:13:03 -0500

    Cambios en el HTML

--- views/header.php
+++ views/header.php
@@ -1,38 +1,43 @@
 <!doctype html>
 <html class="no-js" lang="es">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation | Welcome</title>
-    <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/normalize.css">
-	  <!-- <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css"> -->
-	  <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/foundation.css">
 
-	  <!-- If you are using the gem version, you need this only -->
-	  <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/app.css">
-	  <!-- Insert this within your head tag and after foundation.css -->
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/motion-ui@1.2.3/dist/motion-ui.min.css" />
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Foundation | Welcome</title>
+  <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/normalize.css">
+  <!-- <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css"> -->
+  <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/foundation.css">
 
+  <!-- If you are using the gem version, you need this only -->
+  <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/css/app.css">
+  <!-- Insert this within your head tag and after foundation.css -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/motion-ui@1.2.3/dist/motion-ui.min.css" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,300;0,500;1,300;1,500&display=swap" rel="stylesheet">
   <!--   <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css"> -->
-  </head>
-  <body>
+</head>
 
-    <!-- Start Top Bar -->
-    <div class="navuno top-bar ">
-      <div class="top-bar-left">
-        <ul class="menu">
-          <li class="menu-text">Marketing Site</li>
-          <li><a href="#">One</a></li>
-          <li><a href="#">Two</a></li>
-        </ul>
-      </div>
-      <div class="top-bar-right">
-        <ul class="menu">
-          <li><a href="#">Three</a></li>
-          <li><a href="#">Four</a></li>
-          <li><a href="#">Five</a></li>
-          <li><a href="#">Six</a></li>
-        </ul>
-      </div>
+<body>
+
+  <!-- Start Top Bar -->
+  <div class="navuno top-bar ">
+    <div class="top-bar-left">
+      <ul class="menu">
+        <li class="menu-text">Marketing Site</li>
+        <li><a href="#">One</a></li>
+        <li><a href="#">Two</a></li>
+      </ul>
     </div>
-    <!-- End Top Bar -->
+    <div class="top-bar-right">
+      <ul class="menu">
+        <li><a href="#">Three</a></li>
+        <li><a href="#">Four</a></li>
+        <li><a href="#">Five</a></li>
+        <li><a href="#">Six</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- End Top Bar -->
--- views/main/index.php
+++ views/main/index.php
@@ -12,10 +12,8 @@
 
 	  <!-- Insert this within your head tag and after foundation.css -->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/motion-ui@1.2.3/dist/motion-ui.min.css" />
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!--   <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css"> -->
-
-
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	  <!--   <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css"> -->
   </head>
   <body>
 
@@ -24,13 +22,7 @@
 	  			<img src="<?php echo constant('URL');?>public/img/katari.png" alt="Logo Katari" class="img-logo">
 	  		</div>
 	  		<nav>
-	  			<input type="checkbox" id="check">
-	  			<label for="check" class="checkbtn">
-	  				<i class="fa-solid fa-bars"></i>
-	  			</label>
 	  			<ul>
-		  			<li class="left">
-		  			</li>
 		  			<li><a href="#">Inicio</a></li>
 		  			<li><a href="#">Empresa</a></li>
 		  			<li><a href="#">Productos y Servicios</a></li>
@@ -46,17 +38,32 @@
 
   	<div class="hero">
 
+  		<div class="slider">
+		    <div class="slide current">
+		      <div class="content">
+		        <h3>Curso: ELABORACION DE PLAN ANUAL DE ARCHIVOS 2023-2024</h3>
+		        <p>En coordinacion y apoyo del <span class="resaltado">Archivo Regional de Moquegua</span>, es que se dictará el curso.  Dirigido al personal de Archivos de todas las instituciones.</p>
+		      </div>
+		    </div>
+		    <div class="slide">
+		      <div class="content">
+		        <h3>SERVICIO Y MANTENIMIENTO DE COMPUTADORAS Y LAPTOPS AL 50% DE DESCUENTO</h3>
+		        <p>Aproveche esta oportunidad por apertura, <span class="resaltado">ELIMINACION DE VIRUS, FORMATEOS de PCs y Laptos</span> a <span class="resaltado">Mitad de precio!!</span>.</p>
+		      </div>
+		    </div>
+		    <div class="slide">
+		      <div class="content">
+		        <h3>Servicio: ANALISIS ESTADISTICO PARA TESIS Y MACHINE LEARNIGN</h3>
+		        <p>Se ofrece: Analisis de encuestas, pruebas <span class="resaltado">Estadisticas para Tesis (Chi-cuadrado, Prueba de Hipotesis, etc)</span>, uso de R-Studio y SPSS</p>
+		      </div>
+		    </div>
+		  </div>
 
+		  <div class="buttons">
+		    <button id="prev"><i class="fas fa-arrow-left"></i></button>
+		    <button id="next"><i class="fas fa-arrow-right"></i></button>
+		  </div>
 
-	  	<div class="main">
-	  		<div>
-	  			<h1>Desarrollo de Software a medida de nuestros clientes</h1>
-	  		</div>
-
-	  		<div>
-	  			<p>La mejor solución para su negocio</p>
-	  		</div>
-	  	</div>
   	</div>
 
   	<hr>
@@ -195,86 +202,69 @@
 		</div>
     </div>
 
-	<div class="grid-container  zeta">
-		<div class="row text-center">
-			<h1>NUESTRO PORTAFOLIO</h1>
-		</div>
-		<div class="grid-zeta">
-			<div class="box">
-				<span style="--i:1;"><a href=""><img src="<?php echo constant('URL');?>public/img/image1.jpg" alt=""></a></span>
-				<span style="--i:2;"><a href=""><img src="<?php echo constant('URL');?>public/img/image2.jpg" alt=""></a></span>
-				<span style="--i:3;"><a href=""><img src="<?php echo constant('URL');?>public/img/image3.jpg" alt=""></a></span>
-				<span style="--i:4;"><a href=""><img src="<?php echo constant('URL');?>public/img/image4.jpg" alt=""></a></span>
-				<span style="--i:5;"><a href=""><img src="<?php echo constant('URL');?>public/img/image5.jpg" alt=""></a></span>
-				<span style="--i:6;"><a href=""><img src="<?php echo constant('URL');?>public/img/image6.jpg" alt=""></a></span>
-				<span style="--i:7;"><a href=""><img src="<?php echo constant('URL');?>public/img/image7.jpg" alt=""></a></span>
-				<span style="--i:8;"><a href=""><img src="<?php echo constant('URL');?>public/img/image8.jpg" alt=""></a></span>
-			</div>
-		</div>
-	</div>
 
 
 
+		<!-- Pie de pagina -->
+		<div class="grid-container-fluid foot-er">
+			<div class="grid-container">
+				<div class="grid-x grid-padding-y">
+					<div class="cell small-12 medium-4 large-3 titulo-footer">
+						<h3>Herramientas y servicios</h3>
+						<ul>
+							<li><a href="#" class="foot">Todos los productos</a></li>
+							<li><a href="#" class="foot">Catalogo de productos</a></li>
+							<li><a href="#" class="foot">Costos por servicios</a></li>
+							<li><a href="#" class="foot">Listado de tiendas</a></li>
+							<li><a href="#" class="foot">Ofertas</a></li>
+						</ul>
+					</div>
+					<div class="cell small-12 medium-4 large-3 titulo-footer">
+						<h3>Empresa</h3>
+						<ul>
+							<li><a href="#" class="foot">Blog</a></li>
+							<li><a href="#" class="foot">API</a></li>
+							<li><a href="#" class="foot">Bolsa de Trabajo</a></li>
+							<li><a href="#" class="foot">Mision y Vision</a></li>
+							<li><a href="#" class="foot">Anunciate con nosotros</a></li>
+						</ul>
+					</div>
+					<div class="cell small-12 medium-4 large-3 titulo-footer">
+						<h3>Siganos en Redes Sociales</h3>
+						<a href="#" class="rss"><i class="fa-brands fa-facebook"></i></a>
+						<a href="#" class="rss"><i class="fa-brands fa-tiktok"></i></a>
+						<a href="#" class="rss"><i class="fa-brands fa-linkedin"></i></a>
+						<a href="#" class="rss"><i class="fa-brands fa-youtube"></i></a>
 
-<div class="grid-container-fluid foot-er">
-	<div class="grid-container">
-		<div class="grid-x grid-padding-y">
-			<div class="cell small-12 medium-4 large-3 titulo-footer">
-				<h3>Herramientas y servicios</h3>
-				<ol>
-					<li><a href="#" class="foot">Todos los productos</a></li>
-					<li><a href="#" class="foot">Catalogo de productos</a></li>
-					<li><a href="#" class="foot">Costos por servicios</a></li>
-					<li><a href="#" class="foot">Listado de tiendas</a></li>
-					<li><a href="#" class="foot">Ofertas</a></li>
-				</ol>
-			</div>
-			<div class="cell small-12 medium-4 large-3 titulo-footer">
-				<h3>Empresa</h3>
-				<ol>
-					<li><a href="#" class="foot">Blog</a></li>
-					<li><a href="#" class="foot">API</a></li>
-					<li><a href="#" class="foot">Bolsa de Trabajo</a></li>
-					<li><a href="#" class="foot">Mision y Vision</a></li>
-					<li><a href="#" class="foot">Anunciate con nosotros</a></li>
-				</ol>
-			</div>
-			<div class="cell small-12 medium-4 large-3 titulo-footer">
-				<h3>Siganos en Redes Sociales</h3>
-				<a href="#" class="rss"><i class="fa-brands fa-facebook"></i></a>
-				<a href="#" class="rss"><i class="fa-brands fa-tiktok"></i></a>
-				<a href="#" class="rss"><i class="fa-brands fa-linkedin"></i></a>
-				<a href="#" class="rss"><i class="fa-brands fa-youtube"></i></a>
+					</div>
+					<div class="cell small-12 medium-4 large-3 titulo-footer">
+						<h3>Katari - 2023</h3>
+						<ul>
+							<li><a href="#" class="foot">Terminos y condiciones</a></li>
+							<li><a href="#" class="foot">Privacidad</a></li>
+						</ul>
 
-			</div>
-			<div class="cell small-12 medium-4 large-3 titulo-footer">
-				<h3>Katari - 2023</h3>
-				<ol>
-					<li><a href="#" class="foot">Terminos y condiciones</a></li>
-					<li><a href="#" class="foot">Privacidad</a></li>
-				</ol>
+						<small class="copyright">
+							<span class=""> ©
+			          <script>
+			            document.write(new Date().getFullYear())
+			          </script> KATARI SOFTWARE
+			        </span>
+						Copyrigth  Katari. Inc.</small>
+						<small class="copyright">Todos los derechos reservados.</small>
+						<br>
+						<a href="#">Sitemap</a>
+					</div>
+				</div>
 
-				<small class="copyright">
-					<span class=""> ©
-	          <script>
-	            document.write(new Date().getFullYear())
-	          </script> KATARI SOFTWARE
-	        </span>
-				Copyrigth  Katari. Inc.</small>
-				<small class="copyright">Todos los derechos reservados.</small>
-				<br>
-				<a href="#">Sitemap</a>
+
+
 			</div>
 		</div>
 
 
 
-	</div>
-</div>
-
-
-
-
+	 <script src="<?php echo constant('URL');?>public/js/main.js"></script>
       <script src="<?php echo constant('URL');?>public/js/core/jquery.min.js"></script>
       <script src="<?php echo constant('URL');?>public/js/foundation.min.js"></script>
 
