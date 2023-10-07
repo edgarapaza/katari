f69278e865a691fe00212fb3aa972a0b64c7bcc9
Edgar Apaza Choque <edgarapazac@gmail.com>
2023-10-07 11:07:56 -0500

    Modificando contactos

---
+++ controller/contacto.php
@@ -0,0 +1,15 @@
+<?php
+
+class Contacto extends Controller
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function render()
+	{
+		$this->view->Render('contacto/index');
+	}
+}
---
+++ views/contacto/index.php
@@ -0,0 +1,11 @@
+<?php require ('views/header.php');?>
+
+<div class="grid-container">
+	<div class="grid-x">
+		<h2>Titulo pagina</h2>
+		<img src="<?php echo constant('URL')?>public/img/person2.png" alt="Personal">
+	</div>
+
+</div>
+
+<?php require ('views/footer.php');?>
