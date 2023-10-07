75b1a6c29e7c8f5d7364fd740492eb0da312f9f7
furfuron <143754160+furfurn@users.noreply.github.com>
2023-10-07 11:06:50 -0500

    Agregando cliente

---
+++ controller/clientes.php
@@ -0,0 +1,15 @@
+<?php
+
+class Clientes extends Controller
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function render()
+	{
+		$this->view->Render('clientes/index');
+	}
+}
---
+++ views/clientes/index.php
@@ -0,0 +1,8 @@
+<?php require ('views/header.php');?>
+
+	<div class="grid-container">
+		<!-- Poner el codigo en HTML aqui -->
+
+  
+
+<?php require ('views/footer.php');?>
