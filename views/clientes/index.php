fb305f5ad45475008547c9ce723b174a575ec47c
Jhojan Ichuta Pacco <ichutapaccojhojan@gmail.com>
2023-10-11 10:21:49 -0500

    subiendo canvios de contacto y cliente

--- views/clientes/index.php
+++ views/clientes/index.php
@@ -2,7 +2,73 @@
 
 	<div class="grid-container">
 		<!-- Poner el codigo en HTML aqui -->
+    <div class="grid-container">
+    	<div class="row text-center">
+    		<h2>Clientes</h2>
+    	</div>
 
-  
+    	<div class="grid-x grid-margin-x">
+		    <div class="cell small-12 medium-6 large-4">
+		    	<div class="card">
+					<img src="<?php echo constant('URL')?>public/img/person2.png" alt="Una imagen" class="borde-redondo">
+					  <div class="card-section">
+					    <h4>Nombre</h4>
+					    <p>Nuestra experiencia nos permite culminar proyectos en el tiempo requerido con máxima calidad.</p>
+					  </div>
+					</div>
+		    </div>
+		    <div class="cell small-12 medium-6 large-4">
+		    	<div class="card">
+                <img src="<?php echo constant('URL')?>puBlic/img/person2.png" alt="Una imagen" class="borde-redondo">
+					  <div class="card-section">
+					    <h4>Nombre</h4>
+					    <p>Brindamos precios justos y accesibñes, dependiendo de nuestros planes web. Compara nuestros precios</p>
+					  </div>
+					</div>
+		    </div>
+            <div class="cell small-12 medium-6 large-4">
+		    	<div class="card">
+                <img src="<?php echo constant('URL')?>puBlic/img/person2.png" alt="Una imagen" class="borde-redondo">
+					  <div class="card-section">
+					    <h4>Nombre</h4>
+					    <p>Brindamos precios justos y accesibñes, dependiendo de nuestros planes web. Compara nuestros precios</p>
+					  </div>
+					</div>
+		 </div>
+	</div>
+    </div>
+    <br><br>
+    <div class="grid-container">
+    	<div class="row text-center">
+    		<h2> Proyectos</h2>
+    	</div>
+
+    	<div class="grid-x grid-margin-x">
+		    <div class="cell small-12 medium-6 large-4">
+		    	<div class="card">
+                 <img src="<?php echo constant('URL')?>public/img/image1.jpg" alt="Una imagen">
+					  <div class="card-section">
+					    <h4>Ver</h4>
+					  </div>
+					</div>
+		    </div>
+        <div class="cell small-12 medium-6 large-4">
+		    	<div class="card">
+                <img src="<?php echo constant('URL')?>public/img/image3.jpg" alt="Una imagen">
+					  <div class="card-section">
+					    <h4>Ver</h4>
+					  </div>
+					</div>
+		    </div>
+        <div class="cell small-12 medium-6 large-4">
+		    	<div class="card">
+                <img src="<?php echo constant('URL')?>public/img/image4.jpg" alt="Una imagen">
+					  <div class="card-section">
+					    <h4>Ver</h4>
+					  </div>
+					</div>
+		    </div>
+		</div>
+    </div>
 
 <?php require ('views/footer.php');?>
--- views/contacto/index.php
+++ views/contacto/index.php
@@ -8,10 +8,10 @@
 </div>
 <br>
 
-<div class="grid-x margin-x">
+<div class="grid-x">
 
   <!-- formaulario de contacto-->
-  <div class="columns small-12 large-7">
+  <div class="columns small-12  large-7">
     <form action="../controller/gmail.php" method="POST">
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" name="nombre" required>
@@ -31,7 +31,7 @@
 
   <!-- Datos institucion -->
 
-  <div class="columns small-12 large-4 text-center">
+  <div class="columns small-12 large-5 text-center">
     <table class="responsive-card-table unstriped " >
       <thead>
         <tr>
@@ -72,10 +72,11 @@
     <h6>Horario corrido de 8:00 a.m - 4:00 p.m de lunes a viernes</h6>
     <h2></h2>
 </div>
-<div class="row text-center">
+<!-- <div class="row text-center">
     <h3>Ubicación</h3>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5427.813546723747!2d-70.01611579712919!3d-15.854296913775734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915d6988bf294ca5%3A0xefb4b3011ada2eb4!2sArchivo%20Regional%20de%20Puno!5e0!3m2!1ses-419!2spe!4v1694015263789!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
+</div> -->
 
 </div>
+<br>
 <?php require ('views/footer.php');?>
