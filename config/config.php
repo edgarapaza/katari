cf75925a6f7305bba050f92892948e23d8af5ad6
Edgar Apaza Choque <edgarapazac@gmail.com>
2023-10-11 20:05:23 -0500

    Actualizando

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('URL', 'http://katari.test/');
+define('URL', 'http://localhost/katari/');
 
 define('COMPANY', 'Katari A&C');
 define('HOST', 'localhost');
--- views/contacto/index.php
+++ views/contacto/index.php
@@ -1,30 +1,18 @@
-<?php require ('views/header.php');?>
-<<<<<<< HEAD
-
+<?php require('views/header.php'); ?>
 <div class="grid-container">
-	<div class="grid-x">
-		<h2>Titulo pagina</h2>
-		<img src="<?php echo constant('URL')?>public/img/person2.png" alt="Personal">
-	</div>
+  <!-- codigo en HTML aqui -->
 
-</div>
+  <div class="large text-center">
+    <h1>CONTÁCTANOS</h1>
+    <img src="<?php echo constant('URL'); ?>public/img/imagen.j.png" alt="Una imagen">
+  </div>
+  <br>
 
-<?php require ('views/footer.php');?>
-=======
-	<div class="grid-container">
-		<!-- codigo en HTML aqui -->
+  <div class="grid-x margin-x">
 
-<div class="large text-center">
-  <h1>CONTÁCTANOS</h1>
-  <img src="<?php echo constant('URL')?>puBlic/img/imagen.j.png" alt="Una imagen">
-</div>
-<br>
-
-<div class="grid-x margin-x">
-
-  <!-- formaulario de contacto-->
-  <div class="columns small-12 large-7">
-    <form action="../controller/gmail.php" method="POST">
+    <!-- formaulario de contacto-->
+    <div class="columns small-12 large-7">
+      <form action="../controller/gmail.php" method="POST">
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" name="nombre" required>
 
@@ -38,57 +26,56 @@
         <textarea id="mensaje" name="mensaje" rows="4" required></textarea>
 
         <button class="submit success button" type="submit">ENVIAR</button>
-    </form>
-  </div>
+      </form>
+    </div>
 
-  <!-- Datos institucion -->
+    <!-- Datos institucion -->
 
-  <div class="columns small-12 large-4 text-center">
-    <table class="responsive-card-table unstriped " >
-      <thead>
-        <tr>
-          <th></th>
-          <th>Datos</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>
-          <th>Direccion:</th>
-          <td>
-            <span class="icon-location"></span>
-            ******* ******* ******** ** *
-          </td>
-        <tr>
-          <th>Telefono:</th>
-          <td>
-            <span class="icon-phone"></span>
-            (051) 600 - 465
-          </td>
-        </tr>
-        <tr>
-          <th>Email:</th>
-          <td>
-            <p>
-            <span class="icon-mail"></span>
-            edgarapaza@katari.org.pe
-            </p>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="columns small-12 large-4 text-center">
+      <table class="responsive-card-table unstriped ">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Datos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Direccion:</th>
+            <td>
+              <span class="icon-location"></span>
+              ******* ******* ******** ** *
+            </td>
+          <tr>
+            <th>Telefono:</th>
+            <td>
+              <span class="icon-phone"></span>
+              (051) 600 - 465
+            </td>
+          </tr>
+          <tr>
+            <th>Email:</th>
+            <td>
+              <p>
+                <span class="icon-mail"></span>
+                edgarapaza@katari.org.pe
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
-<br>
-<div class="row text-center">
+  <br>
+  <div class="row text-center">
     <h4>Horario de atencion</h4>
     <h6>Horario corrido de 8:00 a.m - 4:00 p.m de lunes a viernes</h6>
     <h2></h2>
-</div>
-<div class="row text-center">
+  </div>
+  <div class="row text-center">
     <h3>Ubicación</h3>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5427.813546723747!2d-70.01611579712919!3d-15.854296913775734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915d6988bf294ca5%3A0xefb4b3011ada2eb4!2sArchivo%20Regional%20de%20Puno!5e0!3m2!1ses-419!2spe!4v1694015263789!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
+  </div>
 
 </div>
-<?php require ('views/footer.php');?>
->>>>>>> 009173cabc6883241c9044e6ba861a380dac20ce
+<?php require('views/footer.php'); ?>
