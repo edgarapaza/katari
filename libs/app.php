3c787f3bc2fee3eb7954f55b499d5ed5022cd1b0
Edgar Apaza Choque <edgarapazac@gmail.com>
2024-11-16 07:27:24 -0500

    Fusinando

--- libs/app.php
+++ libs/app.php
@@ -7,16 +7,17 @@ class App
   {
     #echo "<p>Nueva APP</p>";
 
-    @$url = $_REQUEST['url'];
+    $url = $_GET['url'];
     $url = rtrim($url, '/');
     $url = explode('/', $url);
 
+
     if (empty($url[0])) {
 
-      $archivoController = "controller/login.php";
+      $archivoController = "controller/main.php";
       require_once $archivoController;
-      $controller = new Login;
-      $controller->loadModel('login');
+      $controller = new main;
+      $controller->loadModel('main');
       $controller->render();
       return false;
     }
@@ -25,32 +26,37 @@ class App
     $archivoController = "controller/" . $url[0] . ".php";
 
     if (file_exists($archivoController)) {
-      #echo "Si existe";
-      require_once $archivoController;
-      $controller = new $url[0];
-      $controller->loadModel($url[0]);
+        #echo "Si existe";
+        require_once $archivoController;
+        $controller = new $url[0];
+        $controller->loadModel($url[0]);
 
-      # numero de elementos del arreglo
-      $nparam = sizeof($url);
+        # numero de elementos del arreglo
+        $nparam = sizeof($url);
 
-      if ($nparam > 1) {
-        if ($nparam > 2) {
-          $param = [];
-          for ($i = 2; $i < $nparam; $i++) {
-            array_push($param, $url[$i]);
+        if($nparam > 1)
+        {
+          if($nparam > 2)
+          {
+            $param = [];
+            for($i = 2; $i < $nparam; $i++)
+            {
+              array_push($param, $url[$i]);
+            }
+            $controller->{$url[1]}($param);
+          }else{
+            $controller->{$url[1]}();
           }
-          $controller->{$url[1]}($param);
-        } else {
-          $controller->{$url[1]}();
+        }else{
+          $controller->render();
         }
+
+
       } else {
+        #echo "no existe";
+        require "controller/error.php";
+        $controller = new ErrorGeneral();
         $controller->render();
       }
-    } else {
-      #echo "no existe";
-      require "controller/error.php";
-      $controller = new ErrorGeneral();
-      $controller->render();
-    }
   }
 }
